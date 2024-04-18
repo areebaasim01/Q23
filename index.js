91debd36698cdb455c34f5800002ff7604f59bb8
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-18 20:15:49 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+"use strict";
+// 23.	Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+Object.defineProperty(exports, "__esModule", { value: true });
+let mobile = "Iphone";
+console.log(mobile == 'Iphone');
+let mobile1 = "Vivo";
+console.log(mobile1 == 'Vivo');
+let mobile2 = "Oppo";
+console.log(mobile2 == 'Oppo');
+let mobile3 = "Sumsung";
+console.log(mobile3 == 'Sumsung');
+let mobile4 = "Nokia";
+console.log(mobile4 == 'Realme');
+let perfumeBrand = "Jimmy Choo";
+console.log(perfumeBrand == "Jimmy Choo");
+let perfumeBrand1 = "Kelvin ";
+console.log(perfumeBrand1 == "Jimmy Choo");
+let tissuePap = "Rose patel";
+console.log(tissuePap == "Xtra kleen");
+let lotion = "Vasline";
+console.log(lotion == "Ponds");
+let clothBrand = "Saphire";
+console.log(clothBrand == "Tariq jameel");
---
+++ index.ts
@@ -0,0 +1,36 @@
+// 23.	Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+let mobile = "Iphone";
+console.log(mobile == 'Iphone');
+
+let mobile1 = "Vivo";
+console.log(mobile1 == 'Vivo');
+
+let mobile2 = "Oppo";
+console.log(mobile2 == 'Oppo');
+
+let mobile3 = "Sumsung";
+console.log(mobile3 == 'Sumsung');
+
+let mobile4 = "Nokia";
+console.log(mobile4 == 'Realme');
+
+let perfumeBrand = "Jimmy Choo"
+console.log(perfumeBrand == "Jimmy Choo");
+
+let perfumeBrand1 = "Kelvin "
+console.log(perfumeBrand1 == "Jimmy Choo");
+
+let tissuePap = "Rose patel"
+console.log(tissuePap == "Xtra kleen");
+
+let lotion= "Vasline"
+console.log(lotion == "Ponds");
+
+let clothBrand = "Saphire"
+console.log(clothBrand == "Tariq jameel");
